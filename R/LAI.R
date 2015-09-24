9c7b9238201b0a89991eb6b4d00a2dab2467a64b
Charles Martin <martin.charles@gmail.com>
2015-09-24 14:32:06 -0400

    Done linting code

--- R/LAI.R
+++ R/LAI.R
@@ -1,48 +1,52 @@
-# Estimate LAI from gap fraction at 57.5 degrees
+#' Estimate LAI from gap fraction at 57.5 degrees
+#' Example usage :
+#' LAI.from.GF.at.angle(calculate.GF(unimodal.threhsold.img("images/IMG_0005.JPG")))
+#' 
 
-# Example usage :
-# LAI.from.GF.at.angle(calculate.GF(unimodal.threhsold.img("images/IMG_0005.JPG")))
-calculate.GF = function(binary.img) { 	
-  freqs = as.data.frame(raster::freq(binary.img))
-  if (nrow(freqs)<2) {
+calculate_GF <- function(binary_img) {
+  freqs <- as.data.frame(raster::freq(binary_img))
+  if (nrow(freqs) < 2) {
     1
   } else {
-    freqs[freqs["value"]==0,"count"] / (freqs[freqs["value"]==0,"count"] + freqs[freqs["value"]==1,"count"])
+    freqs[freqs["value"] == 0,"count"] /
+      (freqs[freqs["value"] == 0,"count"] +
+      freqs[freqs["value"] == 1,"count"])
   }
 }
 
-LAI.from.GF.at.angle = function(
-  GF, 
+LAI_from_GF_at_angle <- function(
+  GF,
   angle=57.5 # degrees
 ) {
-  rad.angle = angle * 0.0174532925
-  
-  return( - (
-    cos(rad.angle) / .5
-  ) * log (GF)	)
+  rad_angle <- angle * 0.0174532925
+
+  return(
+    - (
+      cos(rad_angle) / .5
+      ) * log (GF)
+  )
 }
 
-crop.around.angle = function(
+crop_around_angle <- function(
   img, # raster object
-  camera.horiz.FOV = 73.7, # degrees
+  camera_horiz_FOV = 73.7, # degrees
   # angle at which the camera was pointing
-  focal.angle = 45, # degrees
-  # crop box 
-  crop.top.angle = 57.5+5,
-  crop.bottom.angle = 57.5-5
+  focal_angle = 45, # degrees
+  # crop box
+  crop_top_angle = 57.5 + 5,
+  crop_bottom_angle = 57.5 - 5
 ) {
-  
-  
+
   # pixel/degree ratio
-  pixel.degree.ratio = img@nrows / camera.horiz.FOV
-  
+  pixel_degree_ratio <- img@nrows / camera_horiz_FOV
+
   # at what angle is the picture bottom pointing to
-  bottom.angle = focal.angle - camera.horiz.FOV/2
-  
-  crop.top.pixel = (crop.top.angle - bottom.angle) * pixel.degree.ratio
-  crop.bottom.pixel =(crop.bottom.angle - bottom.angle) * pixel.degree.ratio
-  e = raster::extent(c(0,img@ncols,crop.bottom.pixel,crop.top.pixel) )
-  
+  bottom_angle <- focal_angle - camera_horiz_FOV / 2
+
+  crop_top_pixel <- (crop_top_angle - bottom_angle) * pixel_degree_ratio
+  crop_bottom_pixel <- (crop_bottom_angle - bottom_angle) * pixel_degree_ratio
+  e <- raster::extent(c(0,img@ncols,crop_bottom_pixel,crop_top_pixel) )
+
   return (raster::crop(img,e))
-  
+
 }
