bf0da92a8d672815336322f0e5abcd03da9914fc
Charles Martin <martin.charles@gmail.com>
2015-09-24 15:49:26 -0400

    First series of tests, to make sure we don't break anything

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(LAI)
+
+test_check("LAI")
---
+++ tests/testthat/test.LAI_from_gf_at_57.R
@@ -0,0 +1,44 @@
+library(LAI)
+context("Full LAI computations")
+
+expect_that(
+  LAI_from_gf_at_57(
+    system.file("extdata", "IMG_0005.JPG", package = "LAI")
+  ),
+  equals(0.7, tolerance = .1)
+)
+
+expect_that(
+  LAI_from_gf_at_57(
+    system.file("extdata", "IMG_0307.JPG", package = "LAI")
+  ),
+equals(0)
+)
+
+expect_that(
+  LAI_from_gf_at_57(
+    system.file("extdata", "IMG_9362.JPG", package = "LAI")
+  ),
+  equals(2.8, tolerance = .1)
+)
+
+expect_that(
+  LAI_from_gf_at_57(
+    system.file("extdata", "IMG_9783.JPG", package = "LAI")
+  ),
+  equals(4.4, tolerance = .1)
+)
+
+expect_that(
+  LAI_from_gf_at_57(
+    system.file("extdata", "IMG_9783.JPG", package = "LAI")
+  ),
+  equals(4.4, tolerance = .1)
+)
+
+expect_that(
+  LAI_from_gf_at_57(
+    system.file("extdata", "IMG_7595.JPG", package = "LAI")
+  ),
+  equals(2.6, tolerance = .1)
+)
